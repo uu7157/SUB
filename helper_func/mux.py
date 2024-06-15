20364074cb093717c7af9d1e17cfa9979cac9363
uu7157 <116019430+uu7157@users.noreply.github.com>
2024-06-15 15:17:29 +0300

    Update mux.py

--- helper_func/mux.py
+++ helper_func/mux.py
@@ -107,7 +107,7 @@ async def hardmux_vid(vid_filename, sub_filename, msg):
     command = [
             'ffmpeg','-hide_banner',
             '-i',vid,
-            '-vf','subtitles='+sub+':fontsdir=fonts:force_style="FontName=NotoKufiArabic-Bold\,Fontsize=30"',
+            '-vf','subtitles='+sub+':fontsdir=fonts:force_style="FontName=NotoKufiArabic-Bold\,Fontsize=28"',
             '-c:v','h264',
             '-map','0:v:0',
             '-map','0:a:0?',
